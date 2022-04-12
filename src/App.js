8f25060d67fc57d8a6657fe1a5f84980a1721ed1
roymm <royamunoz@protonmail.com>
2022-04-11 22:01:36 -0600

    Changes simple text for container

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
+import profilePic from './img/profile.jpg'
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Roy Munoz
-    </h1>
-    <h2 className="text-3xl font-bold underline">
-      B54911
-    </h2>
-  </div>
+    <div class="py-8 px-8 max-w-sm mx-auto bg-sky-50 rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={profilePic} alt="Profile" />
+      <div class="text-center space-y-2 sm:text-left">
+        <div class="space-y-0.5">
+          <p class="text-lg text-black font-semibold">Roy Muñoz Miranda</p>
+          <p class="text-slate-500 font-medium">B54911</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
